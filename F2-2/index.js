5f7949fce890c9a57d7cf41057b5e7c304414c95
YinWei <cba5530@gmail.com>
2021-09-07 14:38:05 +0800

    Initial state

---
+++ F2-2/index.js
@@ -0,0 +1,23 @@
+// DEFAULT CODE ////////////////////////
+const BASE_URL = 'https://lyric-api-403c0.firebaseio.com/'
+const songList = document.querySelector('#song-list')
+const lyricsPanel = document.querySelector('#lyrics-panel')
+const album = {
+  artist: 'Adele',
+  album: '25',
+  tracks: [
+    'Hello',
+    'Send My Love (To Your New Lover)',
+    'I Miss You',
+    'When We Were Young',
+    'Remedy',
+    'Water Under the Bridge',
+    'River Lea',
+    'Love in the Dark',
+    'Million Years Ago',
+    'All I Ask',
+    'Sweetest Devotion'
+  ]
+}
+
+// WRITE YOUR CODE ////////////////////////
