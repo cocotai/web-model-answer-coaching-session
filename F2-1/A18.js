c26c314bf236f9e4dc873d810c14b07d3607b9ab
Flora2020 <rossignols1989@gmail.com>
2021-09-14 23:47:02 +0800

    依照建議修改

--- F2-1/A18.js
+++ F2-1/A18.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// 1. 原課程內容提供程式碼使用 let，需要回報課程團隊改用 const，避免學生困惑
 // 資料
 const movies = [{
   title: 'The Avengers',
@@ -21,7 +20,6 @@ const movies = [{
 // 選取節點
 const dataPanel = document.querySelector('#data-panel')
 
-// ======第一階段 開始======
 // 函式
 function displayMovieList (movies) {
   let htmlContent = `
@@ -40,7 +38,7 @@ function displayMovieList (movies) {
     htmlContent += `
         <tr>
           <td>
-            <img src="${movies[i].image}" width="70" class="img-thumbnail" >
+            <img src="${movies[i].image}" width="70" class="img-thumbnail">
           </td>
           <td>${movies[i].title}</td>
           <td>
@@ -77,12 +75,9 @@ function ratingAndDeleteMovie (event) {
       rating -= 1
     }
     ratingBox.textContent = rating
-    // 2. 由於後面程式碼也有用 if 包起來，使得這裡的 return 其實作用不大，建議移除避免學生疑惑
-    return
-  }
 
-  // delete movie
-  if (targetClassList.contains('delete')) {
+    // delete movie
+  } else if (targetClassList.contains('delete')) {
     targetParent.parentElement.remove()
   }
 }
@@ -92,76 +87,3 @@ dataPanel.addEventListener('click', ratingAndDeleteMovie)
 
 // 將組合好的字串傳回節點，修改內容
 dataPanel.innerHTML = displayMovieList(movies)
-// ======第一階段 結束======
-
-// // ======第二階段 開始======
-// // 函式
-// function displayMovieList (movies) {
-//   let htmlContent = `
-//     <table class="table">
-//       <thead>
-//         <tr>
-//           <th>Image</th>
-//           <th>Title</th>
-//           <th>Rating</th>
-//           <th></th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//     `
-//   for (let i = 0; i < movies.length; i++) {
-//     htmlContent += `
-//         <tr>
-//           <td>
-//             <img src="${movies[i].image}" width="70" class="img-thumbnail" >
-//           </td>
-//           <td>${movies[i].title}</td>
-//           <td>
-//             <span class="fa fa-thumbs-up" data-operation="rating" data-rating="add"></span>
-//             <span class="fa fa-thumbs-down px-2" data-operation="rating" data-rating="minus"></span>
-//             <span>${movies[i].rating}</span>
-//           </td>
-//           <td>
-//             <button class="btn btn-sm btn-danger" data-operation="delete">X</button>
-//           </td>
-//         </tr>
-//       `
-//   }
-//   htmlContent += `
-//       </tbody>
-//     </table>
-//   `
-//   return htmlContent
-// }
-
-// // event handler
-// function ratingAndDeleteMovie (event) {
-//   // 3. 雖然 dataset 的做法很好，但似乎 2-2 才會教到，這部分可能也得割愛
-//   // 用 dataset 比對元素
-//   // 考量：class name 主要用於 css 樣式。若把專用於 javascript 的名字寫在 class name 中，若協同合作者一時不察，認為 css 沒有用到該 class name，而把 class name 刪掉，javascript 的功能也會跟著壞掉
-//   const targetDataset = event.target.dataset
-//   const targetParent = event.target.parentElement
-
-//   // rating movie
-//   if (targetDataset.operation === 'rating') {
-//     const ratingBox = targetParent.children[2]
-//     let rating = Number(ratingBox.textContent)
-
-//     if (targetDataset.rating === 'add') {
-//       rating += 1
-//     } else if (rating > 0) {
-//       rating -= 1
-//     }
-//     ratingBox.textContent = rating
-//     return
-//   }
-
-//   // delete movie
-//   if (targetDataset.operation === 'delete') {
-//     targetParent.parentElement.remove()
-//   }
-// }
-
-// dataPanel.addEventListener('click', ratingAndDeleteMovie)
-// dataPanel.innerHTML = displayMovieList(movies)
-// // ======第二階段 結束======
