41d7a236539aad36abcb1afeb8e6887d51f331fc
Charlie <wl00606352@gmail.com>
2021-09-22 17:45:03 +0800

    依照先前2-3BE A4 PR進行修改
    * 修改model路徑名稱
    * 增加種子資料script to package
    * 表單增加星號提示使用者必填

--- F2-3_BackEnd/A6/config/mongoose.js
+++ F2-3_BackEnd/A6/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost/model-restaurant-list", {
+mongoose.connect("mongodb://localhost/restaurant-list", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
