30b50c0e9e8f5d032d6b1ae0584a673e8d18dc27
fornacy <>
2021-10-04 18:39:42 +0800

    code resource setup week1

---
+++ F2-2/TodoList 強化體驗/DOM Event - todolist - 01 init/script.js
@@ -0,0 +1,24 @@
+// 初始變數
+let list = document.querySelector('#my-todo')
+let addBtn = document.querySelector('#addBtn')
+let input = document.querySelector('#newTodo')
+
+// 資料
+const todos = ['Hit the gym', 'Read a book', 'Buy eggs', 'Organize office', 'Pay bills']
+for (let todo of todos) {
+  addItem(todo)
+}
+
+
+// 函式
+function addItem (text) {
+  let newItem = document.createElement('li')
+  newItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="delete fa fa-trash"></i>
+  `
+  list.appendChild(newItem)
+}
+
+
+// write your code here
