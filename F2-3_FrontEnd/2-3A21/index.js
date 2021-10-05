64cc3c236d94e6dcc9d1c6e0b38113851a89a685
Tina <tina126hung@gmail.com>
2021-10-06 00:04:04 +0800

    Finish A21 answer

--- F2-3_FrontEnd/2-3A21/index.js
+++ F2-3_FrontEnd/2-3A21/index.js
@@ -1,5 +1,8 @@
 // target the input element
 const input = document.getElementById("action__input_1");
+// target the dark mode switch element
+const darkModeToggle = document.getElementById("dark__mode__toggle");
+const checkBox = document.querySelector(".table__body");
 
 // click handler
 const toggleMenu = (event) => {
@@ -8,14 +11,7 @@ const toggleMenu = (event) => {
   menu.classList.toggle("hidden");
 };
 
-// bind the event
-input.addEventListener("click", toggleMenu);
-
-// dark mode
-// target the switch element
-const darkModeToggle = document.getElementById("dark__mode__toggle");
-
-// toggle handler
+// dark mode toggle handler
 const darkModeToggleHandler = (event) => {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
@@ -23,6 +19,15 @@ const darkModeToggleHandler = (event) => {
     document.documentElement.setAttribute("data-theme", "light");
   }
 };
+// add the checked class to the row
+const selectTable = (event) => {
+  // 往上找到最近符合條件的 element
+  const parent = event.target.closest('.table__row')
+  parent.classList.toggle("checked")
+};
+
 
 // bind the event
+input.addEventListener("click", toggleMenu);
 darkModeToggle.addEventListener("change", darkModeToggleHandler);
+checkBox.addEventListener("change", selectTable);
