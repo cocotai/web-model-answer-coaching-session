bb35534900dcc153858772a693d51a77dac0ef29
YinWei <cba5530@gmail.com>
2021-10-05 20:38:30 +0800

    按照建議修正並新增Q1範例說明

--- F2-2/A8/Q1/index.js
+++ F2-2/A8/Q1/index.js
@@ -1,9 +1,11 @@
+// Q1_(1)說明：由於按鈕的type = "submit"代表按下按鈕後的預設行為是提交表單，
+// 因此導致畫面尚未按照我們期望的渲染即被重新導向，因此需要透過preventDefault()去阻止表單提交的預設行為。
+
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  // 建議: 不會重新賦值的變數建議使用 const 宣告
-  let name = input.value
+  const name = input.value
   form.innerHTML = `<h3 class="display-3">Hi, ${name}</h3>`
 })
