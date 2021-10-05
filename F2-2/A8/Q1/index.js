7a1185e02bc9242fa0f00e2654ee3055119505e4
yujen <csie.hsu@gmail.com>
2021-10-05 11:42:18 +0800

    建議修正變數宣告

--- F2-2/A8/Q1/index.js
+++ F2-2/A8/Q1/index.js
@@ -3,6 +3,7 @@ const input = document.getElementById('input')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
+  // 建議: 不會重新賦值的變數建議使用 const 宣告
   let name = input.value
   form.innerHTML = `<h3 class="display-3">Hi, ${name}</h3>`
 })
