eaf8fa8757e337433608d3f7a25268734ae2ca3a
Flora2020 <rossignols1989@gmail.com>
2021-10-09 22:41:07 +0800

    catch error

--- F2-2/A1/Q1/main.js
+++ F2-2/A1/Q1/main.js
@@ -1,6 +1,5 @@
 const img = document.querySelector('.dog-image')
 
-axios.get('https://dog.ceo/api/breeds/image/random').then(response => {
-  console.log(response.data)
-  img.src = response.data.message
-})
+axios.get('https://dog.ceo/api/breeds/image/random')
+  .then(response => img.src = response.data.message)
+  .catch(error => console.log(error))
