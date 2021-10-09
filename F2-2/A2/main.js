f3fd3df7eadb2e5b8b689a761ec7da0c3ae8ae3f
Flora2020 <rossignols1989@gmail.com>
2021-10-10 00:44:33 +0800

    只使用 bootstrap 設定樣式

--- F2-2/A2/main.js
+++ F2-2/A2/main.js
@@ -1,33 +1,31 @@
 const panel = document.querySelector('#data-panel')
 const button = document.querySelector('button')
-const url = 'https://randomuser.me/api?gender=female&results=3'
+const userNumber = 3
+const url = `https://randomuser.me/api?gender=female&results=${userNumber}`
 
 button.addEventListener('click', e => {
-  axios.get(url).then(response => {
-    // get data
-    console.log(response)
-    for (let i = 0; i < 3; i++) {
-      const data = response.data.results[i]
-      const user = {
-        name: `${data.name.first} ${data.name.last}`,
-        email: data.email,
-        avatar: data.picture.large
-      }
-      console.log(user)
+  axios.get(url)
+    .then(response => {
+      for (let i = 0; i < userNumber; i++) {
+        // 回傳的資料較為雜亂，先整理成比較整齊的格式
+        const data = response.data.results[i]
+        const user = {
+          name: `${data.name.first} ${data.name.last}`,
+          email: data.email,
+          avatar: data.picture.large
+        }
 
-      // create user template
-      let div = document.createElement('div')
-      let htmlContent = `
-          <div class="user-item">
+        // 把資料加到畫面上
+        const div = document.createElement('div')
+        div.classList.add('d-flex', 'flex-column', 'align-items-center', 'border', 'm-2', 'py-2', 'px-4')
+        const htmlContent = `
             <h3>${user.name}</h3>
-            <img src="${user.avatar}" style="width: 150px;">
+            <img src="${user.avatar}">
             <small>${user.email}</small>
-          </div>
         `
-      console.log(htmlContent)
-      div.innerHTML = htmlContent
-      panel.append(div)
-    }
-  })
+        div.innerHTML = htmlContent
+        panel.append(div)
+      }
+    })
+    .catch(error => console.log(error))
 })
-
