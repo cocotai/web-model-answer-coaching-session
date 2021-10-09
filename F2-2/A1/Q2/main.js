33f780feff3c8a38b90121486e992e6f3d311646
Flora2020 <rossignols1989@gmail.com>
2021-10-09 22:49:35 +0800

    原本的 model answer

---
+++ F2-2/A1/Q2/main.js
@@ -0,0 +1,19 @@
+const panel = document.querySelector('#data-panel')
+const button = document.querySelector('button')
+const url = 'https://randomuser.me/api/'
+
+button.addEventListener('click', e => {
+  axios.get(url).then(response => {
+    console.log(response)
+    let user = response.data.results[0]
+    console.log(user)
+    let name = `${user.name.first} ${user.name.last}`
+    let avatar = user.picture.large
+    let email = user.email
+    panel.innerHTML = `
+      <h3>${name}</h3>
+      <img src="${avatar}">
+      <p>${email}</p>
+    `
+  })
+})
