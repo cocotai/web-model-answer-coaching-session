b507be9c6ed5d6f0c40ec44f6ab8eaa3c0ee4eb5
Max He <wl00887404@gmail.com>
2021-10-12 10:14:50 +0800

    Format F2-2 movie-list-2020 的 renderMovieList

--- F2-2/movie-list-2020/index.js
+++ F2-2/movie-list-2020/index.js
@@ -16,26 +16,36 @@ function renderMovieList(data) {
   let rawHTML = ''
   data.forEach((item) => {
     // title, image, id
-    rawHTML += `<div class="col-sm-3">
-    <div class="mb-2">
-      <div class="card">
-        <img src="${
-          POSTER_URL + item.image
-        }" class="card-img-top" alt="Movie Poster">
-        <div class="card-body">
-          <h5 class="card-title">${item.title}</h5>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${
-            item.id
-          }">More</button>
-          <button class="btn btn-info btn-add-favorite" data-id="${
-            item.id
-          }">+</button>
+    const imageUrl = POSTER_URL + item.image
+
+    rawHTML += `
+      <div class="col-sm-3">
+        <div class="mb-2">
+          <div class="card">
+            <img src="${imageUrl}" class="card-img-top" alt="Movie Poster" />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button 
+                class="btn btn-primary btn-show-movie"
+                data-bs-toggle="modal"
+                data-bs-target="#movie-modal"
+                data-id="${item.id}"
+              >
+                More
+              </button>
+              <button 
+                class="btn btn-info btn-add-favorite"
+                data-id="${item.id}"
+              >
+                +
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>`
+    `
   })
   dataPanel.innerHTML = rawHTML
 }
