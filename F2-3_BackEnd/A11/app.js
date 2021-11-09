4da975c8f479425f11491db0c88de43064efd0b9
Charlie <wl00606352@gmail.com>
2021-11-09 22:42:48 +0800

    simple cookie

--- F2-3_BackEnd/A11/app.js
+++ F2-3_BackEnd/A11/app.js
@@ -1,25 +1,51 @@
 const app = require("express")()
 const express = require("express")
 const exphbs = require("express-handlebars")
+const cookieParser = require("cookie-parser")
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 const PORT = 3000
 
 const validateUser = require("./utils/validator")
 
+// 登入頁面 if 登入過就轉到home頁面
 app.get("/", (req, res) => {
-  res.render("index")
+  if (req.cookies.user) {
+    res.redirect("/home")
+  }
+  return res.render("index")
 })
 
+// 發送登入資料並儲存狀態 via cookie
 app.post("/", (req, res) => {
   const { email, password } = req.body
   const result = validateUser(email, password)
 
   if (!result.status) {
     return res.render("index", { error: result.message })
+  }
+
+  res.cookie("user", { email, password }, { maxAge: 1000 * 60 * 60 * 24 })
+  return res.redirect("/home")
+})
+
+// home頁面 if 沒登入狀態就轉到登入頁面
+// 狀態過期則會清除cookie並轉到登入頁面
+app.get("/home", (req, res) => {
+  console.log(req.cookies)
+  if (!req.cookies.user) {
+    res.redirect("/")
+  }
+
+  const { email, password } = req.cookies.user
+  const result = validateUser(email, password)
+  if (!result.status) {
+    res.clearCookie("user")
+    return res.redirect("/")
   }
 
   return res.render("home", { user: result.user.firstName })
